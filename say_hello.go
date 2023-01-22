ebfae2f01f8cffe3c00ab716322ee65d7f69d511
Aziiz Pranaja <pranaja200102@gmail.com>
2023-01-22 13:50:40 +0700

    update module

--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
 func SayHello() string {
-	return "Hello ini aziiz belajar module go"
+	return "Hello ini aziiz belajar module go dan ini mencoba mengupdate sebuah module"
 }
